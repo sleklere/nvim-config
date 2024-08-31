290d8cd013b18f051bd83dd60c81d5b7849418eb
sleklere <santiagoleklere@gmail.com>
2024-08-31 20:25:25 -0300

    backup JiC

--- lua/sleklere/remap.lua
+++ lua/sleklere/remap.lua
@@ -4,6 +4,16 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- copy without losing what you pasted
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- delete without cutting by default, and <leader>d for cutting
+-- right now, D deletes to the end of the line without cutting, and dd remains unaltered
+vim.keymap.set("n", "D", "\"_D")
+vim.keymap.set("x", "d", "\"_d")
+vim.keymap.set("v", "d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"d")
+vim.keymap.set("n", "<leader>D", "\"D")
+--vim.keymap.set("x", "<leader>d", "\"d")
+vim.keymap.set("v", "<leader>d", "\"d")
+
 -- move selected lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
