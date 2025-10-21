e567baf769242eda004eeecaa8aa8b9abe3e54f2
sleklere <santiagoleklere@gmail.com>
2025-10-21 19:20:24 -0300

    find references and find files commands

--- lua/sleklere/init.lua
+++ lua/sleklere/init.lua
@@ -59,6 +59,7 @@ if not vim.g.vscode then
         callback = function(e)
             local opts = { buffer = e.buf }
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
--- lua/sleklere/lazy/telescope.lua
+++ lua/sleklere/lazy/telescope.lua
@@ -11,7 +11,12 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+            })
+        end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
