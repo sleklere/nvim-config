ceb4fe6808334d8815cdb5a0fbc914b374dd2fa3
sleklere <santiagoleklere@gmail.com>
2025-11-02 17:18:31 -0300

    telescope keymap

--- lua/sleklere/lazy/telescope.lua
+++ lua/sleklere/lazy/telescope.lua
@@ -11,13 +11,13 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', function()
+        vim.keymap.set('n', '<C-p>', function()
             builtin.find_files({
                 hidden = true,
                 no_ignore = true,
             })
         end)
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
