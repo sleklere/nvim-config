df043be21bd5874f4bcd93f3362cd5c3345d26bd
sleklere <santiagoleklere@gmail.com>
2026-04-08 23:01:14 -0300

    feat: remap go to impl

--- lua/sleklere/init.lua
+++ lua/sleklere/init.lua
@@ -63,6 +63,7 @@ if not vim.g.vscode then
             end
             local opts = { buffer = e.buf }
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, opts)
             vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
